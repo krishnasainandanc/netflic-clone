47280058552bd24537fb10507d71f9f7a528e05f
ckrishnasainandan <krishnasai4436@gmail.com>
2022-03-20 18:31:15 +0530

    after remove h1

--- src/App.js
+++ src/App.js
@@ -19,21 +19,18 @@ function App() {
   // //     console.log(res.data.results)
   // //   })
   //  },[])
-  
-   return (
-    <div className="App">
-      <Banner/>
-      <Rows title='NETFLIX ORIGINALS' fetchRequest={re.fetchNetflixOriginals} isLarge={true}/>
-      <Rows title='TRENDING NOW' fetchRequest={re.fetchTrending} isLarge={false}/>
-      <Rows title='COMEDY MOVIES' fetchRequest={re.fetchComedyMovies} isLarge={false}/>
-      <Rows title='HORROR MOVIES' fetchRequest={re.fetchHorrorMovies}/>
-      <Rows title='DOCUMENTARIES MOVIES' fetchRequest={re.fetchDocumentaries}/>
-      <Rows title='ROMANCE MOVIES' fetchRequest={re.fetchRomanceMovies}/>
-      <Rows title='TOP RATED' fetchRequest={re.fetchTopRated}/>
-      <Rows title='ACTION MOVIES' fetchRequest={re.fetchActionMovies}/> 
 
-      <h1>krishna</h1>
-      <h2>sai</h2>
+  return (
+    <div className="App">
+      <Banner />
+      <Rows title='NETFLIX ORIGINALS' fetchRequest={re.fetchNetflixOriginals} isLarge={true} />
+      <Rows title='TRENDING NOW' fetchRequest={re.fetchTrending} isLarge={false} />
+      <Rows title='COMEDY MOVIES' fetchRequest={re.fetchComedyMovies} isLarge={false} />
+      <Rows title='HORROR MOVIES' fetchRequest={re.fetchHorrorMovies} />
+      <Rows title='DOCUMENTARIES MOVIES' fetchRequest={re.fetchDocumentaries} />
+      <Rows title='ROMANCE MOVIES' fetchRequest={re.fetchRomanceMovies} />
+      <Rows title='TOP RATED' fetchRequest={re.fetchTopRated} />
+      <Rows title='ACTION MOVIES' fetchRequest={re.fetchActionMovies} />
     </div>
   );
 }
